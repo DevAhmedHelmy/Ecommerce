4e55fa265dcc3d67c881bf12f47d1027eeaa4599
Ahmed Helmy <helmyahmed248@gmail.com>
2020-08-13 16:10:00 +0200

    ahmed helmy

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ProductRequest extends FormRequest
@@ -13,7 +14,7 @@ class ProductRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,8 +24,36 @@ class ProductRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            //
+        $rules=[
+            'photo' => validate_image(),
+            'category_id' => ['sometimes','nullable'],
+            'tradmark_id' => ['sometimes','nullable'],
+            'manufacthrer_id' => ['sometimes','nullable'],
+            'main_color' => ['sometimes','nullable'],
+            'weight' => ['sometimes','nullable'],
+            'color_id' => ['sometimes','nullable'],
+            'size_id' => ['sometimes','nullable'],
+            'stock' => ['sometimes','nullable'],
+            'price' => ['sometimes','nullable'],
+            'currency_id' => ['sometimes','nullable'],
+            'status' => ['sometimes','nullable'],
+            'refused_reason' => ['sometimes','nullable'],
+            'start_at' => ['sometimes','nullable'],
+            'end_at' => ['sometimes','nullable'],
+            'start_offer_at' => ['sometimes','nullable'],
+            'end_offer_at' => ['sometimes','nullable'],
+            'offer_price' => ['sometimes','nullable'],
+
+            'additionals' => ['sometimes','nullable'],
+            'malls' => ['sometimes','nullable'],
+
         ];
+        foreach (config('translatable.locales') as $locale) {
+
+            $rules += $this->isMethod('post') ? [$locale . '.name' => ['required','unique:product_translations,name']]: [$locale . '.name' => ['required', Rule::unique('product_translations', 'name')->ignore($this->product->id,'product_id')]];
+            $rules +=  [$locale . '.content' => ['required']];
+
+        }
+        return $rules;
     }
 }
