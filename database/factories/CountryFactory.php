b1b6d9c877482cc554e2fec94646d70928c11135
Ahmed Helmy <helmyahmed248@gmail.com>
2020-08-26 01:18:19 +0200

    ahmed helmy

--- database/factories/CountryFactory.php
+++ database/factories/CountryFactory.php
@@ -6,7 +6,12 @@ use App\Models\Country;
 use Faker\Generator as Faker;
 
 $factory->define(Country::class, function (Faker $faker) {
+    $faker = Faker\Factory::create('ar_SA');
     return [
+        'name' => $faker->country ,
+        'currency' => $faker->currency,
+        'iso_code' => $faker->country,
+        'code' => $faker->country
 
     ];
 });
